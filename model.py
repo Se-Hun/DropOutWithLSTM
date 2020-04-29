a6445077afccd2d42cc377beae3b65e207a7397b
Se-Hun <tpgns0539@naver.com>
2020-04-30 08:26:19 +0900

    model

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class IMDBLstm(nn.Module):
         self.bs = bs
         self.nl = nl
         self.emb = nn.Embedding(vocab, hidden_size)
-        self.rnn = nn.LSTM(hidden_size, hidden_size, nl)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, nl)
         self.fc2 = nn.Linear(hidden_size, n_cat)
         self.softmax = nn.LogSoftmax(dim=-1)
 
@@ -18,11 +18,11 @@ class IMDBLstm(nn.Module):
         bs = inp.size()[1]
         if bs != self.bs:
             self.bs = bs
-        e_out = self.emb(inp)
+        e_out = self.emb(inp) # 먼저, embedding하고
         h0 = c0 = Variable(e_out.data.new(*(self.nl, self.bs, self.hidden_size)).zero_())
-        rnn_o, _ = self.rnn(e_out, (h0, c0))
-        rnn_o = rnn_o[-1]
-        fc = F.dropout(self.fc2(rnn_o), p=0.5)
+        lstm_out, _ = self.lstm(e_out, (h0, c0))
+        lstm_out = lstm_out[-1]
+        fc = F.dropout(self.fc2(lstm_out), p=0.5)
         print(fc)
         # 여기 부분에 dropout 결과를 저장하는 코드를 넣어서 살펴봐야할듯?
         return self.softmax(fc)
